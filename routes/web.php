cf487f52f1d3d20592a9e15af75c9044a25c99ad
Bakhara Alief <ara.bakhara12@gmail.com>
2022-10-20 15:45:07 +0700

    feature(landing page): update landing page (#3)
    * add assets and modif welcome page
    * update welcome page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+// Route::get('/', fn () => dd( asset('assets/img/favicon.png')) );
 
 Route::get('/', function () {
     return view('welcome');
