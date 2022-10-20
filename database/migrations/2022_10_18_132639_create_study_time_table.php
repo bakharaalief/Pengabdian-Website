f5120b0601ac17b894e4677974042289c56fe684
Bakhara Alief <ara.bakhara12@gmail.com>
2022-10-20 15:59:19 +0700

    feature(auth): add migration in auth (#2)
    * add user level migration
    * new update
    * feature(landing page): update landing page (#3)
    * add assets and modif welcome page
    * update welcome page

--- database/migrations/2022_10_18_132639_create_study_time_table.php
+++ database/migrations/2022_10_18_132639_create_study_time_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateStudyTimeTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('study_time', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('password');
-            $table->foreignId('user_level')
-                ->references('id')
-                ->on('user_level')
-                ->onUpdate('cascade')
-                ->onDelete('restrict');
             $table->timestamps();
         });
     }
@@ -33,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('study_time');
     }
 }
