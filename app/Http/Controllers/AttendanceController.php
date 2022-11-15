2cf8362863bba3eccfd88edda53e279ee9977dc2
ranggasuryap15 <ranggasurya.313@gmail.com>
2022-11-15 10:19:37 +0700

    attendance controller

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Kelas;
+use App\Models\StudyTime;
+use App\Models\Attendance;
 
 class AttendanceController extends Controller
 {
     function index()
     {
-        return view('attendance.index');
+        $classes = Kelas::all();
+        $studyTime = StudyTime::all();
+        $attendances = Attendance::where('id');
+        return view('attendance.index')->with(compact('classes', 'studyTime'));
     }
 }
